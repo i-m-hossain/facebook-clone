fde6464edbefe91256526555682c35090d8a0e07
Md. Imran Hossain <mdimran1409036@gmail.com>
2021-03-02 21:47:53 +0600

    images_can_be_uploaded

--- app/User.php
+++ app/User.php
@@ -39,6 +39,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function images(){
+
+        return $this->hasMany(UserImage::class);
+    }
 
     public function likedPosts()
     {
